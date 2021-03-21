ebc8ec45079bff2c578e87201944e01de27634b3
zjdklfjlret <81071992+zjdklfjlret@users.noreply.github.com>
2021-03-21 08:20:15 +0000

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@
     res.writeHead(200, {
       'Content-Type': 'text/plain'
     });
-    return res.end("Welcome to Heroku https://github.com/onplus/shadowsocks-heroku");
+    return res.end("Welcome to Heroku https://github.com/zjdklfjlret/shadowsocks-heroku");
   });
 
   wss = new WebSocketServer({
